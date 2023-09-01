2b861587be5b6a088e008afeb58a738eff934d86
JannatunNimeNishat <nishatnime100@gmail.com>
2023-09-01 22:45:39 +0600

    get destination by category api added

--- index.js
+++ index.js
@@ -13,44 +13,59 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const destinationCollection = client.db('airbnbDB').collection('destinations');
+
+        // get all destinations
+        app.get('/all_destinations', async (req, res) => {
+            const destinations = await destinationCollection.find().toArray();
+            res.send(destinations);
+        });
+
+        
+
+        // get destinations by category
+        app.get('/destinations_by_category/:destination_value', async(req,res)=>{
+            const destination_value = req.params.destination_value;
+            const query = {category: destination_value}
+            const destinationByCategory = await destinationCollection.find(query).toArray();
+          
+            res.send(destinationByCategory);
+        });
 
 
 
-
-
-
-
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 
 
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('airbnb server is running');
 })
 
 
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`airbnb server is running at port ${port}`);
 })
 
